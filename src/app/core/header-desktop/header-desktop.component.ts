b7e9bcb68186e75097cda6befc8037a22bbc02de
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2024-03-09 15:25:19 +0200

    modal functionality is implemented as a method of the component class

--- src/app/core/header-desktop/header-desktop.component.ts
+++ src/app/core/header-desktop/header-desktop.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { desktopModal } from '../../../scripts/desktop-shopping-cart';
+import { Component, OnInit, ViewChild } from '@angular/core';
+// import { desktopModal } from '../../../scripts/desktop-shopping-cart';
+import { ShoppingCartDesktopComponent } from 'src/app/shared/shopping-cart-desktop/shopping-cart-desktop.component';
 
 @Component({
   selector: 'app-header-desktop',
@@ -7,7 +8,23 @@ import { desktopModal } from '../../../scripts/desktop-shopping-cart';
   styleUrls: ['./header-desktop.component.css']
 })
 export class HeaderDesktopComponent implements OnInit {
+  @ViewChild('modal') private modalCart!: ShoppingCartDesktopComponent;
+  public isShown: boolean = false;
+  
   ngOnInit(): void {
-    desktopModal();
+    // desktopModal();
+  }
+
+  modalCartCalc() {
+    this.modalCart.cartCalc();   
+  }
+
+  modalToggle(): void {
+    console.log('HERE');    
+    
+    this.isShown = !this.isShown;
+    this.modalCartCalc();
+    console.log(this.isShown);    
+    // return this.isShown;
   }
 }
