ee6b584595cdac0fabb634924551eb4ffd3e2ae3
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2024-11-26 22:02:58 +0200

    rework details page's logic and style

--- src/app/shared/product-details/product-details.component.ts
+++ src/app/shared/product-details/product-details.component.ts
@@ -2,19 +2,20 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subscription, catchError } from 'rxjs';
+import { Observable, Subscription, catchError, of } from 'rxjs';
 
 import { environment } from 'src/environments/environment.development';
 
 import { UserForAuth } from 'src/app/types/user';
 
-import { Belt, Blazer, Boot, Bottom, Cap, CartItem, Glove, Gym, Hat, Jacket, Legging, Longwear, Outdoors, Partywear, Running, Ski, Slippers, Snowboard, Sunglasses, Surf, Sweater, Swim, Tie, Trainers, Tuxedo, Waistcoat, Watch, initialItem } from 'src/app/types/item';
+import { Belt, Blazer, Boot, Bottom, Cap, CartItem, Glove, Gym, Hat, Item, Jacket, Legging, Longwear, Outdoors, Partywear, Running, Ski, Slippers, Snowboard, Sunglasses, Surf, Sweater, Swim, Tie, Trainers, Tuxedo, Waistcoat, Watch, initialItem } from 'src/app/types/item';
 import { ShoppingCartStateManagementService } from '../state-management/shopping-cart-state-management.service';
 import { ShoppingCartService } from '../shopping-cart/shopping-cart.service';
 
 import { HttpLogoutInterceptorSkipHeader } from 'src/app/interceptors/http-logout.interceptor';
 import { HttpAJAXInterceptorSkipHeader } from 'src/app/interceptors/http-ajax.interceptor';
 import { UserStateManagementService } from '../state-management/user-state-management.service';
+import { ErrorsService } from '../errors/errors.service';
 
 const BASE_URL = `${environment.apiDBUrl}/data`;
 
@@ -34,7 +35,7 @@ export class ProductDetailsComponent implements OnInit, AfterViewInit, OnDestroy
     Gym | Running | Ski | Snowboard | Swim | Surf | Outdoors | Bottom | Legging | Sweater |
     Blazer | Jacket | Waistcoat | Tuxedo | Partywear | Tie = initialItem;
   public cartItemsCounter = 0;
-  public defImgOpacity = 1;
+  public selectedImgUrl = '';
 
   public form: FormGroup = this.fb.group({
     fgItem: this.fb.group({
@@ -66,13 +67,13 @@ export class ProductDetailsComponent implements OnInit, AfterViewInit, OnDestroy
     private route: ActivatedRoute,
     private fb: FormBuilder,
     private http: HttpClient,
+    private errorsService: ErrorsService,
     private router: Router,
     private render: Renderer2,
     private cartService: ShoppingCartService,
     private cartStateMgmnt: ShoppingCartStateManagementService
   ) { this.cartItemsCounter = this.cartStateMgmnt.getCartItemsCount() }
 
-  @ViewChildren('imgElements') private imgElements!: QueryList<ElementRef>;
   @ViewChildren('spanColorElements') private spanColorElements!: QueryList<ElementRef>;
 
   get itemCtrlsGr() {
@@ -106,64 +107,42 @@ export class ProductDetailsComponent implements OnInit, AfterViewInit, OnDestroy
 
     const detailsSubscription = this.getItem(url, id)
       .pipe(
-        catchError(err => { throw err; })
+        catchError(err => { return of(err); })
       )
-      .subscribe({
-        next: (itm) => {
-          this.loading = false;
-          // this.item = itm;
-          const { _ownerId, _id, _createdOn, image, altImages, cat, subCat, description, size, color, brand, quantity, price } = itm;
-          // const buyed = this.cartItms$$.value.some(itm => itm._id == _id);
-          // const inCart = this.cartItms.some(itm => itm._id == _id);
-          this.item = { _ownerId, _id, _createdOn, image, altImages, cat, subCat, description, size, color, brand, quantity, price };
-          // console.log(itm);
-          // console.log(this.item);
-          // console.log(this.cartItms$$.value);
-          // const propsArr = Object.entries(itm);
-          // console.log(propsArr);
-          // propsArr.forEach(([k, v]) => {
-          //   (this.form.get('fgItem') as FormGroup).addControl(k, new FormControl(v, Validators.required));
-          // });
-          this.itemCtrlsGr.patchValue({
-            _ownerId,
-            _id,
-            _createdOn,
-            image,
-            altImages,
-            cat,
-            subCat,
-            description,
-            size,
-            color,
-            brand,
-            quantity,
-            price,
-          });
-          (this.formInitalValue.get('fgItem') as FormGroup).patchValue({ ...this.itemCtrlsGr.value });
-          // console.log({ ...this.itemCtrlsGr.value });
-          // console.log(this.formInitalValue.get('fgItem')?.value);
-        },
-        error: err => {
-          this.loading = false;
-          this.httpErrorsArr = [...this.httpErrorsArr, { ...err }];
-          console.log(err);
-          console.log(this.httpErrorsArr);
+      .subscribe(res => {
+        this.loading = false;
+        if (res instanceof HttpErrorResponse) {
+          this.errorsService.sethttpErrorsArrState([...this.httpErrorsArr, { ...res }]);
+          this.httpErrorsArr = [...this.httpErrorsArr, { ...res }];
+          return;
         }
+        const { _ownerId, _id, _createdOn, image, altImages, cat, subCat, description, size, color, brand, quantity, price } = res as Item;
+        this.item = { _ownerId, _id, _createdOn, image, altImages, cat, subCat, description, size, color, brand, quantity, price };
+        this.itemCtrlsGr.patchValue({
+          _ownerId,
+          _id,
+          _createdOn,
+          image,
+          altImages,
+          cat,
+          subCat,
+          description,
+          size,
+          color,
+          brand,
+          quantity,
+          price,
+        });
+        (this.formInitalValue.get('fgItem') as FormGroup).patchValue({ ...this.itemCtrlsGr.value });
+        // console.log({ ...this.itemCtrlsGr.value });
+        // console.log(this.formInitalValue.get('fgItem')?.value);
       });
     this.unsubscriptionArray.push(detailsSubscription);
   }
 
   ngAfterViewInit(): void {
-    // console.log(this.spanCol);
-    // console.log(this.img);
     const spanElementsSubscription = this.spanColorElements.changes.subscribe((els: QueryList<ElementRef>) => {
-      // console.log(els.length);
-      // els.forEach(el => {
-      //   console.log(el.nativeElement);
-      // });
       els.forEach((el, i) => {
-        // console.log(el.nativeElement);
-        // console.log(this.itemCtrlsGr.get('color')?.value[i]);
         this.render.setStyle(el.nativeElement, 'background-color', this.itemCtrlsGr.get('color')?.value[i]);
       });
     });
@@ -195,17 +174,8 @@ export class ProductDetailsComponent implements OnInit, AfterViewInit, OnDestroy
     >(`${BASE_URL}/${url}/${id}`, { headers });
   }
 
-  selectColor(): void {
-    this.imgElements.forEach(el => {
-      // console.log(el.nativeElement.dataset['image']);
-      // console.log(this.selectedColor?.value);
-      if (el.nativeElement.dataset['image'] == this.selectedColor?.value) {
-        this.defImgOpacity = 0;
-        el.nativeElement.classList.contains('active') ? null : this.render.addClass(el.nativeElement, 'active');
-      } else {
-        el.nativeElement.classList.contains('active') ? this.render.removeClass(el.nativeElement, 'active') : null;
-      }
-    });
+  selectColor(idx: number): void {
+    this.selectedImgUrl = this.item.altImages[idx];
   }
 
   public trackByUrl(index: number, url: string): string {
@@ -221,10 +191,8 @@ export class ProductDetailsComponent implements OnInit, AfterViewInit, OnDestroy
     const { _ownerId, _id, _createdOn, image, altImages, cat, subCat, description, brand, size, selectedSize, color, selectedColor, quantity, selectedQuantity, price } = this.itemCtrlsGr.value as CartItem;
     const product = selectedQuantity * price;
     this.cartService.addCartItem({ _ownerId, _id, _createdOn, image, altImages, cat, subCat, description, brand, size, selectedSize, color, selectedColor, quantity, selectedQuantity, price, product, checked: false });
-    // this.item = { ...this.item, inCart: true };
     this.itemCtrlsGr.reset({ ...this.formInitalValue.get('fgItem')?.value });
-    this.imgElements.forEach(el => el.nativeElement.classList.contains('active') ? this.render.removeClass(el.nativeElement, 'active') : null);
-    this.defImgOpacity = 1;
+    this.selectedImgUrl = this.item.image;
     this.cartItemsCounter++;
     // console.log(this.cartItms$$.value);
   }
