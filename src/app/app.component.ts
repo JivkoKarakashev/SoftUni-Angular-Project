8f0c65082938bb2667b370270fcee398815c3b29
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2025-01-15 11:51:22 +0200

    Add user welcome text to the navigation bar; Minor changes to details page style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { UserStateManagementService } from './shared/state-management/user-state-management.service';
+import { UserForAuth } from './types/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ecommerce-app-Angular-v16';
+
+  private userSub: Subscription;
+  public user: UserForAuth | null = null;
+  public guest = 'Guest';
+
+  constructor(private userStateMgmnt: UserStateManagementService) {
+    this.userSub = Subscription.EMPTY;
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.userStateMgmnt.getUserState().subscribe(usr => this.user = usr);
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSub) {
+      this.userSub.unsubscribe();
+    }
+    // console.log('UnsubArray = 1');
+  }
 }
